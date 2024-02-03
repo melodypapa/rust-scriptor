4e43823efd59ce4b6388e5e104c79fac8bc30ecf
melodypapa <melodypapa@outlook.com>
2024-02-03 18:51:18 +0800

    feature_2: fix  the test

--- tests/y2s_test.rs
+++ tests/y2s_test.rs
@@ -2,7 +2,7 @@ use assert_cmd::Command;
 
 #[test]
 fn run(){
-    //let mut cmd = Command::cargo_bin("y2s").unwrap();
+    let mut cmd = Command::cargo_bin("y2s").unwrap();
 
-    //cmd.assert().success();
+    cmd.assert().failure();
 }
