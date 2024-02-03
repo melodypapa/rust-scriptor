f3778e0795bced00561a9e7282ba87e14eae1e5e
melodypapa <melodypapa@outlook.com>
2024-02-03 18:21:19 +0800

    Update the travis config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 //use std::io;
 
+use rust_scriptor::scriptor::ScriptorDoc;
+
 fn main() {
     println!("Hello, world!");
 
     //let mut guess = String::new();
     //io::stdin().read_line(&mut guess).expect("Failed to read line");
     //println!("{}", guess);
+
+    let doc = ScriptorDoc::new("test", "test");
+
+    println!("{:?}", doc);
 
     another_function();
 }
