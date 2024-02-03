0ddf5d4e9751131966b5447b00edb768bef563be
melodypapa <melodypapa@outlook.com>
2024-02-03 22:42:51 +0800

    create the scriptor xml template

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-
 mod models;
 
 pub use crate::models::scriptor;
-
+pub use crate::models::front_of_house;
 
--- src/models/back_of_house.rs
+++ src/models/back_of_house.rs
@@ -27,3 +27,17 @@ mod back_of_house {
 
     fn cook_order() {}
 }
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
